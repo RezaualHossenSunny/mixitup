964ab7d2e8a5a44d872e7c33105ed0e7a63d66fe
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-03-23 02:35:50 +0600

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,99 @@
 import { useState } from "react"
 import './App.css'
+import react from './assets/react.png'
+import img1 from './assets/mongo.jpg'
+import img2 from './assets/next.png'
+import img3 from './assets/express.png'
+import img4 from './assets/nodejs.png'
+import img5 from './assets/git.png'
+import img6 from './assets/figma.png'
+import img7 from './assets/js.png'
+import img8 from './assets/redux.png'
+import img9 from './assets/ant.jpg'
 
 function App() {
-const [click,setClick]=useState(1) 
+  const [click, setClick] = useState(1)
 
-function update(id){
-setClick(id)
-}
+  function update(id) {
+    setClick(id)
+  }
   return (
     <>
       <div className="max-w-container mx-auto">
 
-      <div className="flex gap-16 py-7">
+        <div className="flex gap-16 py-7">
 
-      <p onClick={()=> update(1)} className="border-2 border-[#7CFC00] py-4 px-4 bg-[#7CFC00] tex-1xl font-serif font-bold text-[#2F4F4F] cursor-pointer rounded-2xl">language</p>
-      <p onClick={()=> update(2)} className="border-2 border-[#7CFC00] py-4 px-4 bg-[#7CFC00] tex-1xl font-serif font-bold text-[#2F4F4F] cursor-pointer rounded-2xl">language1</p>
-      
-      
+          <p onClick={() => update(1)} className="w-[150px] text-center border-2 border-[#050038] py-4 px-4 bg-[#050038] tex-1xl font-serif font-bold text-white cursor-pointer rounded-2xl hover:bg-violet-600 ">Web</p>
+
+          <p onClick={() => update(2)} className="w-[150px] text-center border-2 border-[#050038] py-4 px-4 bg-[#050038] tex-1xl font-serif font-bold text-white cursor-pointer rounded-2xl hover:bg-violet-600 ">Tools</p>
+
+          <p onClick={() => update(3)} className="w-[150px] text-center border-2 border-[#050038] py-4 px-4 bg-[#050038] tex-1xl font-serif font-bold text-white cursor-pointer rounded-2xl hover:bg-violet-600 ">Programming
+          </p>
+          <p onClick={() => update(4)} className="w-[150px] text-center border-2 border-[#050038] hover:bg-violet-600  py-4 px-4 bg-[#050038] tex-1xl font-serif font-bold text-white cursor-pointer rounded-2xl">Others
+          </p>
+
+
+
+
+        </div>
+
+        <div className="mt-16 ">
+          <div className={click === 1 ? 'show' : "setshow"}>
+            <div className="grid grid-cols-6 gap-12">
+              <img src={react} />
+              <img src={img1} />
+              <img src={img2} />
+              <img src={img3} />
+              <img src={img4} />
+
+
+            </div>
+          </div>
+
+
+          <div className={click === 2 ? 'show' : "setshow"}>
+            <div className="grid grid-cols-6 gap-12">
+
+              <img src={img5} />
+              <img src={img6} />
+
+
+
+            </div>
+          </div>
+
+          <div className={click === 3 ? 'show' : "setshow"}>
+            <div className="grid grid-cols-6 gap-12">
+
+              <img src={img7} />
+
+
+
+
+            </div>
+          </div>
+
+          <div className={click === 4 ? 'show' : "setshow"}>
+            <div className="grid grid-cols-6 gap-12">
+
+              <img src={img8} />
+              <img src={img9} />
+
+
+
+
+            </div>
+          </div>
+
+
+
+        </div>
+
+
+
       </div>
 
-    
 
-      </div>
-
-      <div className="mt-1">
-        <div className={click === 1? 'show':"setshow" }>sss</div>
-        <div className={click === 2? 'show':"setshow" }>djddjdj</div>
-      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ export default {
     extend: {
       maxWidth: {
         'container': '1140px',
-      }
+      },
+      colors: {
+        'comon': '#050038',
+        'rgba': 'rgba(5, 0, 56, 0.40);',
+        'second': 'rgba(5, 0, 56, 0.60);'
+      },
     },
   },
   plugins: [],
